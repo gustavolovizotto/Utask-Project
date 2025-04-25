45bdc6675390a7e667dc9cb868c6d81b6444a19c
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-04-25 01:37:59 -0300

    Cria a estrutura da página principal

---
+++ src/mainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+export function MainPage () {
+    return (
+        <div className="container">
+            <div className="header" />
+            <div className="content">
+                <h1>Bem-vindo à uTask 3.0</h1>
+                <p>Gerencie suas tarefas de forma eficiente e produtiva.</p>
+                <img 
+                    className="logo" 
+                    src="/src/assets/Ilustração cadastro.svg" 
+                    alt="Logo" 
+                />
+            </div>
+        </div>
+    );
+}
