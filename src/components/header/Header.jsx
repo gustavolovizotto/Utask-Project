7e8897ee404f75d39cbc08587a29c54d5bb2e1ea
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-04-25 01:37:30 -0300

    Cria o header da mainpage

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from'./header.module.css'
+export function Header (){
+    return(
+        <header>
+        <div className={styles.container}>
+            <img src="/src/assets/[Imagem] Logo branca.svg" alt="Logo Branca da Unect" className="logo-unect" />
+            <img src="/src/assets/uTask 3.0 lgo.svg" alt="Logo da uTask 3.0" className="utask-logo" />
+            <img src='/src/assets/[Botão] Light mode.svg' alt="Botão do lightmode" className="light-btn" />
+        </div>
+        </header>
+    )
+}
