e30b3e55a400ff74b4e01e8d78ab7b47d255645e
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-04-25 22:03:42 -0300

    troca para browseRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Header } from './components/header/Header'
 import { Footer } from './components/footer/Footer'
 import { Login } from './login/Login'
@@ -8,13 +8,13 @@ import { LoginForm } from './components/loginForm/LoginForm'
 function App() {
 
   return (
-        <HashRouter>
+        <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LoginForm />} />
         </Routes>
-        </HashRouter>
+        </BrowserRouter>
   )
 }
 export default App
