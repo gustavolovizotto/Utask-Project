133756dcb3719cffdf2e508e8688c52742612f3f
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-04-25 21:58:59 -0300

    Troca browseRouter para HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Header } from './components/header/Header'
 import { Footer } from './components/footer/Footer'
 import { Login } from './login/Login'
@@ -8,13 +8,13 @@ import { LoginForm } from './components/loginForm/LoginForm'
 function App() {
 
   return (
-        <BrowserRouter>
+        <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LoginForm />} />
         </Routes>
-        </BrowserRouter>
+        </HashRouter>
   )
 }
 export default App
