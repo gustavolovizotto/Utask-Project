187d4d4e5c6b3323797920a927f6d20e84cf09b4
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-04-30 14:01:04 -0300

    Cria o componente respectivo ao header do kanban

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,38 @@
-import styles from'./header.module.css'
-export function Header (){
-    return(
-        <header>
-        <div className={styles.container}>
-            <img src="/src/assets/[Imagem] Logo branca.svg" alt="Logo Branca da Unect" className="logo-unect" />
-            <img src="/src/assets/uTask 3.0 lgo.svg" alt="Logo da uTask 3.0" className="utask-logo" />
-            <img src='/src/assets/[Botão] Light mode.svg' alt="Botão do lightmode" className="light-btn" />
-        </div>
-        </header>
-    )
+
+import styles from './header.module.css';
+
+export function Header({ isDarkMode, toggleDarkMode }) {
+  return (
+    <div className={styles.header} style={{ '--header-bg': isDarkMode ? '#333' : '#226ED8' }}>
+        
+        
+        <img
+          src={
+            isDarkMode
+              ? '/src/assets/[Imagem] Logo azul unect (1).svg'
+              : '/src/assets/[Imagem] Logo branca.svg'
+          }
+          alt="Logo Unect"
+        />
+
+        <h1 className={styles.title} style={{ '--header-text-color': isDarkMode ? '#226ED8' : '#FAFAFA' }}>uTask 3.0</h1>
+    
+
+
+      <button
+        type="button"
+        className={styles.darkmode_toggle}
+        onClick={toggleDarkMode}
+      >
+        <img
+          src={
+            isDarkMode
+              ? '/src/assets/[Botão] dark mode (1).svg'
+              : '/src/assets/[Botão] Light mode.svg'
+          }
+          alt="Toggle dark mode"
+        />
+      </button>
+    </div>
+  );
 }
