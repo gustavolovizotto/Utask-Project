c8b076e986e64ae275b67f249269c4ecac948956
Gustavo Lovizotto Tesin <gustavolovizotto@gmail.com>
2025-05-07 13:40:24 -0300

    Ajusta o footer e o conteúdo da mainPage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,8 @@ export function Footer ()  {
     return(
         <footer>
             <div className={styles.container}>
-                <p>© Processo de Trainee </p>
-                <p className='unect'>Unect Jr.</p>
+                <p>© Processo de Trainee <a  target="_blank" href='https://unect.com.br/ '>Unect Jr.</a></p>
+            
                 <p>Feito com ❤ por Gustavo Tesin</p>
             </div>
         </footer>
